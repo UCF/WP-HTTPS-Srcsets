71512483e0de413c37927c1598fd21e86741d59f
Jo Dickson <jogreybill@gmail.com>
2016-03-03 14:08:33 -0600

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,15 @@
+=== Srcset HTTPS Fix ===
+Contributors: ucfwebcom
+Tags: srcset, https
+Requires at least: 4.4
+Tested up to: 4.4.2
+Stable tag: 1.0.0
+
+This plugin forces https to be used in srcset values for responsive images.
+
+== Description ==
+
+Fixes a bug exposed in WordPress 4.4 that causes images to use http srcset urls incorrectly over https connections, resulting in insecure content errors and images not loading.  This plugin sets all srcset value url schemas to use https.
+
+Fix provided by WPTavern:
+http://wptavern.com/how-to-fix-images-not-loading-in-wordpress-4-4-while-using-ssl
---
+++ srcset-https-fix.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Srcset HTTPS fix
+Description: Forces all responsive srcset urls to use HTTPS (fixes bug exposed in WP 4.4:  https://core.trac.wordpress.org/ticket/25449#comment:13)
+Version: 0.0.0
+Author: UCF Web Communications
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/*
+ * Force URLs in srcset attributes into HTTPS scheme.
+ * http://wptavern.com/how-to-fix-images-not-loading-in-wordpress-4-4-while-using-ssl
+ */
+function ssl_srcset( $sources ) {
+  foreach ( $sources as &$source ) {
+    $source['url'] = set_url_scheme( $source['url'], 'https' );
+  }
+
+  return $sources;
+}
+add_filter( 'wp_calculate_image_srcset', 'ssl_srcset' );
+
+?>
